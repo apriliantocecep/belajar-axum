84bded4551690351b4213df47149742efbf88e51
Cecep Aprilianto <cecepaprilianto@gmail.com>
2025-02-05 13:35:26 +0700

    feat: start listening routes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::SocketAddr;
+use axum::response::Html;
+use axum::Router;
+use axum::routing::get;
+
+#[tokio::main]
+async fn main() {
+    let routes_hello = Router::new().route(
+        "/hello",
+        get(|| async { Html("Hello <b>World</b>") })
+    );
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 7878));
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    println!("Listening on {}", listener.local_addr().unwrap());
+
+    axum::serve(listener, routes_hello).await.unwrap()
 }
